fc123a6abcfcdb4c6a1ea890f8841bbd3e9504a6
Aaron Schaer <aaschaer@uchicago.edu>
2019-02-04 17:32:28 -0600

    add unit test for new paging method and iter_key

--- tests/unit/test_transfer_paging.py
+++ tests/unit/test_transfer_paging.py
@@ -15,7 +15,13 @@ class PagingSimulator(object):
         self.n = n  # the number of simulated items
 
     def simulate_get(
-        self, path, params=None, headers=None, response_class=None, retry_401=True
+        self,
+        path,
+        params=None,
+        headers=None,
+        response_class=None,
+        response_kwargs=None,
+        retry_401=True,
     ):
         """
         Simulates a paginated response from a Globus API get supporting limit,
@@ -38,6 +44,45 @@ class PagingSimulator(object):
         response._content = six.b(json.dumps(data))
         response.headers["Content-Type"] = "application/json"
         return IterableTransferResponse(response)
+
+    def simulate_get_shared_endpoint_list(
+        self,
+        path,
+        params=None,
+        headers=None,
+        response_class=None,
+        response_kwargs=None,
+        retry_401=True,
+    ):
+        """
+        Simulates a paginated response from GET shared_endpoint_list
+        which uses a different paging style and a non DATA key
+
+        Uses simplified next_token logic where next_token is the first int
+        to return on the next page
+        """
+        data = {}
+        next_token = params.get("next_token") or 0
+        max_results = 10  # limited for ease of testing
+
+        # if we are capped by n
+        if next_token + max_results >= self.n:
+            shared_endpoints = [{"id": i} for i in range(next_token, self.n)]
+            next_token = None
+
+        # if we are capped by max results
+        else:
+            shared_endpoints = [
+                {"id": i} for i in range(next_token, next_token + max_results)
+            ]
+            next_token = next_token + max_results
+
+        # make the simulated response
+        data = {"shared_endpoints": shared_endpoints, "next_token": next_token}
+        response = requests.Response()
+        response._content = six.b(json.dumps(data))
+        response.headers["Content-Type"] = "application/json"
+        return IterableTransferResponse(response, **(response_kwargs or {}))
 
 
 @pytest.fixture
@@ -141,3 +186,54 @@ def test_iterable_func(paging_simulator):
 
     with pytest.raises(StopIteration):
         six.next(generator)
+
+
+def test_shared_endpoint_iteration(paging_simulator):
+    """
+    Confirms PaginatedResource handles the non standard
+    formatting of the shared_endpoint_list response
+    """
+    # num_results < n
+    less_results = N - 7
+    pr_less = PaginatedResource(
+        paging_simulator.simulate_get_shared_endpoint_list,
+        "path",
+        {"params": {}},
+        paging_style=PaginatedResource.PAGING_STYLE_TOKEN,
+        iter_key="shared_endpoints",
+        num_results=less_results,
+    )
+    # confirm results
+    for item, expected in zip(pr_less.data, range(less_results)):
+        assert item["id"] == expected
+
+    assert pr_less.num_results_fetched == less_results
+
+    # num_results > n
+    more_results = N + 7
+    pr_more = PaginatedResource(
+        paging_simulator.simulate_get_shared_endpoint_list,
+        "path",
+        {"params": {}},
+        paging_style=PaginatedResource.PAGING_STYLE_TOKEN,
+        iter_key="shared_endpoints",
+        num_results=more_results,
+    )
+    # confirm results
+    for item, expected in zip(pr_more.data, range(N)):
+        assert item["id"] == expected
+    assert pr_more.num_results_fetched == N
+
+    # num_results = None (fetch all)
+    pr_none = PaginatedResource(
+        paging_simulator.simulate_get_shared_endpoint_list,
+        "path",
+        {"params": {}},
+        paging_style=PaginatedResource.PAGING_STYLE_TOKEN,
+        iter_key="shared_endpoints",
+        num_results=None,
+    )
+    # confirm results
+    for item, expected in zip(pr_none.data, range(N)):
+        assert item["id"] == expected
+    assert pr_none.num_results_fetched == N
